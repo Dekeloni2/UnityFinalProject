f4f8a4d004a932818a36679caaf2c7f4363513ac
Dekeloni2 <dekelmail34@gmail.com>
2026-03-29 08:04:19 +0300

    More final puzzle work

---
+++ Assets/LaserDestroyingScene.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LaserDestroyScene : MonoBehaviour
+{
+    public CutsceneText cutscene;
+    private bool triggered = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (triggered)
+            return;
+
+        if (other.CompareTag("MagneticObject"))
+        {
+            triggered = true;
+
+            StartCoroutine(cutscene.ShowMultiple(
+                "Damn, I probably needed that box",
+                "What is this? Another button?",
+                "Lets press it and see what happens"));
+        }
+    }
+}
